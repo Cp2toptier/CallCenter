befcf8fb3f5c8cbebaae0a8af54d8147bc102d7d
Cp2toptier <lopezguillermo89@gmail.com>
2015-01-15 19:25:24 +0100

    Update Default.aspx.cs

--- Callcenter/Default.aspx.cs
+++ Callcenter/Default.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +12,9 @@ namespace Callcenter
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            MembershipUser userX = Membership.GetUser();
+            Guid userIdX = userX == null ? Guid.Empty : (Guid)userX.ProviderUserKey;
+            Label1.Text = userIdX.ToString();
         }
     }
 }
