15ae1ee37178885bf2e7fa3f978c91a203cd81a4
Cp2toptier <lopezguillermo89@gmail.com>
2015-01-15 19:45:11 +0100

    Update Default.aspx.designer.cs

--- Callcenter/Default.aspx.designer.cs
+++ Callcenter/Default.aspx.designer.cs
@@ -11,13 +11,5 @@ namespace Callcenter {
     
     
     public partial class _Default {
-    /// <summary>
-        /// Control Label1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
